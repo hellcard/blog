ca636a70d12c571193658fa1c57e626b597f6084
w7 <w7@fcksociety>
2024-11-24 17:20:27 +0800

    big update

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name = 'about'),
     path('projects/', views.projects, name = 'projects'),
+    path('projects/<int:cat>/', views.projects_by_id, name = 'projects_ids'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Projects, Category
 
 def about(request):
     return render(request, 'blog/about.html')
 
 def projects(request):
-    return render(request, 'blog/projects.html')
+    projs_1 = Projects.objects.all()[:2]
+    projs_2 = Projects.objects.all()[2:4]
+    projs_3 = Projects.objects.all()[4:6]
+    projs_4 = Projects.objects.all()[6:]
+    categories = Category.objects.all()
+    return render(request, 'blog/projects.html', {'projs_1' : projs_1, 'projs_2' : projs_2, 'projs_3' : projs_3, 'projs_4' : projs_4, 'categories' : categories})
+
+def projects_by_id(request, cat):
+    current_category = Category.objects.get(pk = cat)
+    projects = Projects.objects.filter(category = cat)
+    categories = Category.objects.all()
+    return render(request, 'blog/projects_id.html', {'current_category' : current_category, 'projects' : projects, 'categories' : categories})
+
+def main_page(request):
+    return render(request, 'main_page.html')
