47242223c0ce98087d905c73744ee0e6bef6c871
w7 <w7@fcksociety>
2024-11-24 13:48:07 +0800

    update website: ',' was added :/

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('about/', views.about, name = 'about'),
-    path('projects/', views.projects, name = 'projects')
+    path('projects/', views.projects, name = 'projects'),
 ]
